ce42b3359c9a0afa3e3db3c3f95bb17d186d3a4d
iDovy <dovydas.simonovas@gmail.com>
2018-02-14 18:45:32 +0200

    Most of game complete
    Only lose screen and lose implementation

--- game_show_app_v1/js/app.js
+++ game_show_app_v1/js/app.js
@@ -32,31 +32,55 @@ function addPhraseToDisplay(arr) {
 
 function checkLetter(button){
     let letters = document.querySelectorAll(".letter");
+    var correct = false;
     for (let i = 0; i < letters.length; i++) {
         if (letters[i].textContent == button.textContent) {
             letters[i].className += " show";
+            correct = true;
         }
-        else{
-            return null;
-        }
+        
     }
-}
+    if (correct) {
+        
+    }
+    else{
+        missed++;
+        document.querySelector(".try").setAttribute("src", "images/lostHeart.png");
+        document.querySelector(".try").removeAttribute("class");
 
+    }
+};
 
 // Start
 
 getRandomPhraseAsArray(phrases);
 addPhraseToDisplay(phraseArray);
-
-start.addEventListener( "click", () => {
-    start.parentElement.style.display = "none";
-});
-
+    start.addEventListener( "click", () => {
+        start.parentElement.style.display = "none";
+    });
+    
 letters.addEventListener( "click", (e) => {
     if (e.target.nodeName == "BUTTON") {
         let key = e.target;
         let letterFound = checkLetter(key);
         key.className += " chosen";
         key.setAttribute("disabled", "");
+        if (missed >= 5) {
+            var x = document.querySelectorAll(".chosen");
+            x.classList.remove("chosen");
+            x.removeAttribute("disabled");
+            console.log("niet");
+        }
+        let letters2 = document.querySelectorAll(".letter");
+        let shown = document.querySelectorAll(".show");
+        if (letters2.length == shown.length) {
+            start.parentElement.classList.remove("start");
+            start.parentElement.setAttribute("class", "win");
+            start.parentElement.style.display = "inline";
+            start.innerHTML = "Yay";
+            start.addEventListener( "click", () => {
+                window.location = "index.html"
+            });
+        }
     }
-})
+});
