b7d634fe758280029cb0b2d867a7498ff9abb6be
iDovy <dovydas.simonovas@gmail.com>
2018-02-14 10:57:15 +0200

    Game kinda working
    Only first letter appears

--- game_show_app_v1/js/app.js
+++ game_show_app_v1/js/app.js
@@ -1,8 +1,62 @@
 var letters = document.getElementById("qwerty");
 var phrase = document.getElementById("phrase");
 var missed = 0;
-var start = document.getElementById("start")
+var start = document.getElementById("start");
+var game = document.querySelector("#phrase ul");
+var phrases =[
+    "javascript is cool",
+    "this is a word guessing game",
+    "the greatest phrase of all phrases",
+    "a wheel of success",
+    "i hope you wont get this one"
+];
+
+function getRandomPhraseAsArray(arr){
+    var i = Math.floor(Math.random() * 5);
+    var array = arr[i].split("")
+    return array;
+};
+
+const phraseArray = getRandomPhraseAsArray(phrases);
+
+function addPhraseToDisplay(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        const li = document.createElement("li")
+        li.textContent = arr[i];
+        game.appendChild(li);
+        if (game.lastElementChild.textContent != " ") {
+            game.lastElementChild.className += " letter";
+        }
+    }
+};
+
+function checkLetter(button){
+    let letters = document.querySelectorAll(".letter");
+    for (let i = 0; i < letters.length; i++) {
+        if (letters[i].textContent == button.textContent) {
+            letters[i].className += " show";
+        }
+        else{
+            return null;
+        }
+    }
+}
+
+
+// Start
+
+getRandomPhraseAsArray(phrases);
+addPhraseToDisplay(phraseArray);
 
 start.addEventListener( "click", () => {
     start.parentElement.style.display = "none";
 });
+
+letters.addEventListener( "click", (e) => {
+    if (e.target.nodeName == "BUTTON") {
+        let key = e.target;
+        let letterFound = checkLetter(key);
+        key.className += " chosen";
+        key.setAttribute("disabled", "");
+    }
+})
